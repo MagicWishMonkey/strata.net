e0cb5bd71e81f846883326ba7b5e91a216ede170
Ron Rodenberg <rodenberg@gmail.com>
2015-03-04 15:13:20 -0600

    Tweaks and farts.

---
+++ Debug.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Strata {
+    public class Debug{
+        static void Main(string[] args) {
+            Console.Write("DEBUG!");
+        }
+    }
+}
